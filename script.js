c7d7b76eef6d0894d9cfaab2d3c90ec88e171e3e
Sam Bardsley <sam.m.bardsley@gmail.com>
2022-05-22 13:47:21 -0700

    Icon now changes with theme

--- script.js
+++ script.js
@@ -25,9 +25,10 @@ function guessingGame(){
   btn.addEventListener('click', function handleClick() {
     if (document.body.className == "dark") {
       document.body.className = "light";
-      
+      btn.querySelector('ion-icon').setAttribute('name', 'moon-outline');
     } else {
       document.body.className = "dark";
+      btn.querySelector('ion-icon').setAttribute('name', 'sunny-outline');
     }
   });
   
